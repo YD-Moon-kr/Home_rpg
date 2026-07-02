33236c45eea9045890ca634ea5899bb9121cddb5
dudemr018@gmail.com <dudemr018@gmail.com>
2026-07-02 23:56:43 +0900

    feat: Add force app update cache-buster button to bypass aggressive mobile PWA caching

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chore-rpg-v17'; // 캐시 버전을 v17로 상향하여 JS 선언부 호이스팅 참조 에러 픽스 버전 배포
+const CACHE_NAME = 'chore-rpg-v18'; // 캐시 버전을 v18로 상향하여 강제 업데이트 캐시 파괴(Cache Busting) 기능 버전 배포
 const ASSETS = [
   './',
   './index.html',
